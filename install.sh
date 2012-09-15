be7d41bd97fad0930d7bd4c6ef657a272943aea0
oniltonmaciel <oniltonmaciel@gmail.com>
2012-09-15 18:25:00 -0300

    Added variables for script paths
    This variables helps if the user needs to change the default paths, and also makes easier to change the names of the directories and files. This script works with the default ubuntu install

--- install.sh
+++ install.sh
@@ -1,16 +1,58 @@
 #!/bin/bash
+
+#==== Munin dirs / default config ====#
+
+	munin_conf_dir="/etc/munin"
+	munin_html_dir="/var/www/munin"
+	munin_tmpl_dir="$munin_conf_dir/templates"
+	
+	#Deal with ubuntu path case (it uses cache)
+	if [ ! -d /var/www/munin/ ]
+	then
+	    munin_html_dir="/var/cache/munin/www/"
+	fi
+	
+	munin_conf_fpath="$munin_conf_dir/munin.conf"
+
+	path_munin_conf="$munin_conf_dir/munin.conf"
+
+#==== Mobile Munin default config ====#
+	
+	#Config file
+	mob_conf="munin-mobile.conf"
+
+	#Mobile Template dir
+	mob_tmpl_dir="templates-mobiles"
+
+	#Mobile HTML dir
+	src_mob_html_dir="mobile-www"
+	dest_mob_html_dir="mobile"	
+	
+	#Mobile cron script 
+	mob_script="update-mobile.sh"
+
+#=== Final install paths ===#
+
+	path_conf="$munin_conf_dir/$mob_conf_file"
+	path_tmpl="$munin_conf_dir/$mob_tmpl_dir"
+	path_html="$munin_html_dir/$dest_mob_html_dir"
+	path_script="$munin_conf_dir/$mob_cron_script"
+
+
+#=== Program start ===#
+
 echo ""
 echo "Mobile Munin Template Installer"
 echo ""
 echo "This will create the following directories: "
-echo " ./var/cache/munin/www/mobile" #for ubuntu other was ./var/www/munin/mobile
-echo " ./etc/munin/templates-mobiles"
+echo " .$path_html" 
+echo " .$path_tmpl"
 echo ""
 echo "And copy the following files:"
-echo " ./munin-mobile.conf   -> /etc/munin/munin-mobile.conf"
-echo " ./templates-mobiles/* -> /etc/munin/templates-mobiles/"
-echo " ./mobile-www/*        -> /var/cache/munin/www/mobile/*"
-echo " ./update-mobile.sh    -> /etc/munin/update-mobile.sh"
+echo " ./$mob_conf   -> $path_conf"
+echo " .$mob_tmpl_dir/* -> $path_tmpl/"
+echo " .$src_mob_html_dir/*        -> $path_html/*"
+echo " ./$mob_cron_script    -> $path_script"
 echo ""
 echo "It also sets the permissions to the same as your munin files"
 echo ""
@@ -40,42 +82,43 @@ else
         exit
 fi
 
-if [ -f /etc/munin/munin-mobile.conf ]
+if [ -f $path_conf ]
 then
-    echo "-  /etc/munin/munin-mobile.conf       - file exists! (not copied)"
+    echo "-  $path_conf       - file exists! (not copied)"
 else
-    echo "- ./munin-mobile.conf -> /etc/munin/munin-mobile.conf"
-    cp `pwd`/munin-mobile.conf /etc/munin/munin-mobile.conf    
-    chown /etc/munin/munin-mobile.conf --reference=/etc/munin/munin.conf
+    echo "- ./$mob_conf -> $path_conf"
+    cp `pwd`/$mob_conf $path_conf
+    chown $path_conf --reference=$path_munin_conf
 fi    
 
-echo "- ./templates-mobiles                 -> /etc/munin/templates-mobiles"
+echo "- ./$mob_tmpl_dir                 -> $path_tmpl"
 
-if [ ! -d /etc/munin/templates-mobiles ]
+if [ ! -d $path_tmpl ]
 then
-    mkdir /etc/munin/templates-mobiles
+    mkdir $path_tmpl
 fi
 
-cp `pwd`/templates-mobiles/* /etc/munin/templates-mobiles/
-chown -R /etc/munin/templates-mobiles --reference=/etc/munin/templates
+cp `pwd`/$mob_tmpl_dir/* $path_tmpl/
+chown -R $path_tmpl --reference=$munin_tmpl_dir
 
-echo "- ./mobile-www                        -> /var/www/munin/mobile"
+echo "- ./$src_mob_html_dir                   	     -> $path_html"
 
-if [ ! -d /var/cache/munin/www/mobile ]
+if [ ! -d $path_html ]
 then
-    mkdir /var/cache/munin/www/mobile
+    mkdir $path_html
 fi
 
-cp -r `pwd`/mobile-www/* /var/cache/munin/www/mobile
-chown -R /var/cache/munin/www/mobile --reference=/var/cache/munin/munin #was /var/www/munin
+cp -r `pwd`/$src_mob_html_dir/* $path_html 
+chown -R $path_html --reference=$munin_html_dir 
 
-echo "- ./update-mobile.sh                 -> /etc/munin/update-mobile.sh"
+echo "- ./$mob_script                 -> $path_script"
 
-cp update-mobile.sh /etc/munin/
-chown /etc/munin/update-mobile.sh --reference=/etc/munin/munin-mobile.conf
+cp $mob_script $path_script
+chown $path_script --reference=$path_conf
 
-chmod 755 /etc/munin/update-mobile.sh
+chmod 755 $path_script
 
 echo ""
 echo "Finished!"
+
 
